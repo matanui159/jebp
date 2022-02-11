4d3ffb5435d642ef7d8325453d54a6960e2b4bdf
matanui159 <jasmine@minteronline.com>
2022-02-11 21:36:13 +1000

    Move ASAN_OPTIONS to ENV

--- dev/toolchain/aarch64-linux-gnu.cmake
+++ dev/toolchain/aarch64-linux-gnu.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR armv8)
+set(CMAKE_SYSTEM_PROCESSOR arm64)
 set(prefix aarch64-linux-gnu)
 set(CMAKE_C_COMPILER ${prefix}-gcc)
-set(CMAKE_CROSSCOMPILING_EMULATOR qemu-aarch64 -L /usr/${prefix} -R 0)
+set(CMAKE_CROSSCOMPILING_EMULATOR qemu-aarch64 -L /usr/${prefix})
